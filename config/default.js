6164bbbb2b1de20f1ee1588e46c7e1a71de420b9
Tim Nolet <tim@magnetic.io>
2017-10-26 20:51:51 +0200

    make it different

--- config/default.js
+++ config/default.js
@@ -4,8 +4,8 @@ config.bind = '0.0.0.0'
 config.port = 3000
 
 config.version = require('../package').version
-config.backgroundColour = process.env.SIMPLE_SERVICE_BGCOLOR || 'cornflowerblue'
-config.heroText = process.env.SIMPLE_SERVICE_HERO_TEXT || 'This looks great!'
+config.backgroundColour = process.env.SIMPLE_SERVICE_BGCOLOR || 'darkseagreen'
+config.heroText = process.env.SIMPLE_SERVICE_HERO_TEXT || 'But this is even better!'
 // throws a 500 error on each request
 config.shouldAlwaysError = process.env.SIMPLE_SERVICE_SHOULD_ALWAYS_ERROR || false
 // throws a 500 randomly
