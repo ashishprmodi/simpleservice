d8cda799a02995efb57be04b4b928937fa9a2305
Tim Nolet <tim@magnetic.io>
2017-11-21 13:59:17 +0100

    make /events relative

--- static/app.js
+++ static/app.js
@@ -4,7 +4,7 @@ function handleClick () {
     event: 'click'
   }
   const request = new XMLHttpRequest()
-  const url = '/events'
+  const url = 'events'
   request.open('POST',url)
   request.setRequestHeader('Content-Type', 'application/json')
   request.send(JSON.stringify(clickEvent))
